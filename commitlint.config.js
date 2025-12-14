965715d04da29f33d9d4a2ca0c2640803a4cdb56
Diego Hoyos <diego.hoyos@perficient.com>
2025-12-13 21:47:45 -0500

    mal

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    // Tu regla personalizada para el límite de longitud
     "header-max-length": [2, "always", 72],
   },
 };

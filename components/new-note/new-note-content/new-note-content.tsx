262f051c67d67f8ef57c3794c98218aa7764543a
HoyosDiego <dfhoyos29@gmail.com>
2026-01-05 23:26:00 -0500

    feat(app): get counting notes and notes by limit

--- components/new-note/new-note-content/new-note-content.tsx
+++ components/new-note/new-note-content/new-note-content.tsx
@@ -1,61 +1,60 @@
 import { ColorOpacity } from "@/constants";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, Text, TouchableWithoutFeedback, View } from "react-native";
 import { InputText } from "../../ui";
 import { Button } from "../../ui/button/button";
 import { HeaderNewNote } from "../header-new-note";
 import { styles } from "./new-note-content.style";
 import { INewNoteContentProps } from "./new-note-content.types";
 
-export function NewNoteContent({
-  setNote,
-  note,
-  onPress,
-}: INewNoteContentProps) {
+export function NewNoteContent({ setNote, note, onPress }: INewNoteContentProps) {
   return (
-    <View style={styles.headerContainer}>
-      <HeaderNewNote />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.headerContainer}>
+          <HeaderNewNote />
 
-      <ScrollView contentContainerStyle={styles.formContainer}>
-        <Text style={[styles.label, { backgroundColor: note.bgcolor }]}>
-          Título
-        </Text>
+          <ScrollView
+            contentContainerStyle={styles.formContainer}
+            keyboardShouldPersistTaps="handled" >
+            <Text style={[styles.label, { backgroundColor: note.bgcolor }]}>Título</Text>
+            <InputText
+              placeholder="Dale un nombre a tu nota..."
+              value={note.title}
+              onChangeText={(text) => setNote({ ...note, title: text })}
+              containerStyle={{
+                ...styles.inputTitle,
+                borderColor: ColorOpacity(note.bgcolor, 80),
+              }}
+              maxLength={20}
+            />
 
-        <InputText
-          placeholder="Dale un nombre a tu nota..."
-          value={note.title}
-          onChangeText={(text) => setNote({ ...note, title: text })}
-          containerStyle={{
-            ...styles.inputTitle,
-            borderColor: ColorOpacity(note.bgcolor, 80),
-          }}
-          maxLength={20}
-        />
+            <Text style={[styles.label, { backgroundColor: note.bgcolor }]}>Descripción</Text>
+            <InputText
+              placeholder="Escribe aquí tus ideas..."
+              value={note.content}
+              onChangeText={(text) => setNote({ ...note, content: text })}
+              multiline
+              containerStyle={{
+                ...styles.textArea,
+                borderColor: ColorOpacity(note.bgcolor, 80),
+              }}
+              textAlignVertical="top"
+            />
 
-        <Text style={[styles.label, { backgroundColor: note.bgcolor }]}>
-          Descripción
-        </Text>
-
-        <InputText
-          placeholder="Escribe aquí tus ideas..."
-          value={note.content}
-          onChangeText={(text) => setNote({ ...note, content: text })}
-          multiline
-          containerStyle={{
-            ...styles.textArea,
-            borderColor: ColorOpacity(note.bgcolor, 80),
-          }}
-          textAlignVertical="top"
-        />
-
-        <Button
-          style={{ ...styles.saveButton, backgroundColor: note.bgcolor }}
-          onPress={onPress}
-          disabled={!note.title.trim() || !note.content.trim()}
-        >
-          <Text style={styles.saveButtonText}>GUARDAR NOTA</Text>
-        </Button>
-      </ScrollView>
-    </View>
+            <Button
+              style={{ ...styles.saveButton, backgroundColor: note.bgcolor }}
+              onPress={onPress}
+              disabled={!note.title.trim() || !note.content.trim()}
+            >
+              <Text style={styles.saveButtonText}>GUARDAR NOTA</Text>
+            </Button>
+          </ScrollView>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- hooks/useAppDB.ts
+++ hooks/useAppDB.ts
@@ -1,30 +1,33 @@
-import { fetchNotes, getUser, initDB, INote, NoteInput, saveNote, saveUser, User, UserInput } from '@/services';
-import { isDbLoadedAtom, notesListAtom, userAtom } from '@/state';
+import { countAllNotes, fetchNotes, getUser, initDB, INote, NoteInput, saveNote, saveUser, User, UserInput } from '@/services';
+import { isDbLoadedAtom, notesListAtom, totalNotesCountAtom, userAtom } from '@/state';
 import { useSetAtom } from 'jotai';
 import { useCallback, useEffect } from 'react';
 
 export const useAppDB = () => {
     const setNotesList = useSetAtom(notesListAtom);
+    const setCountNotes = useSetAtom(totalNotesCountAtom);
     const setUser = useSetAtom(userAtom);
     const setIsDbLoaded = useSetAtom(isDbLoadedAtom);
 
     useEffect(() => {
         const loadInitialData = async () => {
             try {
-                await initDB(); 
-                
-                const [notes, user] = await Promise.all([
+                await initDB();
+
+                const [allNotes, notes, user] = await Promise.all([
+                    countAllNotes(),
                     fetchNotes(),
-                    getUser() ,
+                    getUser(),
                 ]);
-                
+
                 setNotesList(notes);
+                setCountNotes(allNotes);
                 setUser(user);
-                
+
             } catch (error) {
                 console.error("Error while start query for user:", error);
             } finally {
-                setIsDbLoaded(true); 
+                setIsDbLoaded(true);
             }
         };
 
@@ -32,33 +35,33 @@ export const useAppDB = () => {
     }, [setNotesList, setUser, setIsDbLoaded]);
 
 
-   
+
     const saveUserToDb = useCallback(async (userInput: UserInput) => {
         await saveUser(userInput);
-        
-        const updatedUser: User = { 
-            id: 1, 
+
+        const updatedUser: User = {
+            id: 1,
             ...userInput
         } as User;
 
-        setUser(updatedUser); 
+        setUser(updatedUser);
         return updatedUser;
 
     }, [setUser]);
 
-  
+
     const saveNoteToDb = useCallback(async (noteObject: NoteInput): Promise<INote> => {
-        const finalId = await saveNote(noteObject); 
-        
-        const updatedNote: INote = { 
-            ...noteObject, 
-            id: finalId, 
-            timestamp: Date.now() 
-        } as INote; 
-        
+        const finalId = await saveNote(noteObject);
+
+        const updatedNote: INote = {
+            ...noteObject,
+            id: finalId,
+            timestamp: Date.now()
+        } as INote;
+
         setNotesList(prevNotes => {
             const existingIndex = prevNotes.findIndex(n => n.id === finalId);
-            
+
             if (existingIndex > -1) {
                 const newNotes = [...prevNotes];
                 newNotes[existingIndex] = updatedNote;
@@ -67,11 +70,11 @@ export const useAppDB = () => {
                 return [updatedNote, ...prevNotes];
             }
         });
-        
+
         return updatedNote;
     }, [setNotesList]);
 
-    
+
     return {
         saveUserToDb,
         saveNoteToDb,
